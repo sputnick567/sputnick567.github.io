0634d16e496f7e5d9a7ecf9bf970d41625742ff1
sputnick567 <77747127+sputnick567@users.noreply.github.com>
2022-07-05 17:42:08 +0200

    redirect to info project

--- docs/JS/main.js
+++ docs/JS/main.js
@@ -1,3 +1,6 @@
+window.location.href = "https://discord-bot-sputnick567.herokuapp.com/";
+
+
 
 if (Math.random() < 0.35) {
     document.getElementById("formsUmfrage").style.visibility = "visible";
